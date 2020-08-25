81a8b8d35c7d5df04422233f05556ff76cbf3fa0
wlgud0402 <wlgudrlgus@naver.com>
2020-08-25 19:13:27 +0900

    use jinja

--- ch3/polls/views.py
+++ ch3/polls/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from polls.models import Question
+
 
 # Create your views here.
+def index(request):
+    latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_question_list':latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request):
+    return HttpResponse("안녕")
+
+def results(request):
+    return HttpResponse("메롱")
+
+def vote(request):
+    return HttpResponse("뇸뇸")
