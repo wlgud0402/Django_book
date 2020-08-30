b102af3613d889c5f2f98bb0bd674b8635891dca
wlgud0402 <wlgudrlgus@naver.com>
2020-08-30 16:00:24 +0900

    templates extends

--- ch3/books/models.py
+++ ch3/books/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField('Author')
@@ -27,5 +27,4 @@ class Publisher(models.Model):
 
     def __str__(self):
         return self.name
-
         
--- ch3/books/urls.py
+++ ch3/books/urls.py
@@ -1,26 +1,27 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'books'
 urlpatterns = [
-    #/books/
+    # /books/
     path('', views.BooksModelView.as_view(), name='index'),
 
-    #/books/book/
+    # /books/book/
     path('book/', views.BookList.as_view(), name='book_list'),
 
-    #/books/author/
+    # /books/author/
     path('author/', views.AuthorList.as_view(), name='author_list'),
 
-    #/books/publisher/
-    path('publisher/', views.Publisher.as_view(), name='publisher_list'),
+    # /books/publisher/
+    path('publisher/', views.PublisherList.as_view(), name='publisher_list'),
 
-    #books/book/99/
-    path('book<int:pk>', views.BookDetail.as_view(), name='book_detail'),
+    # /books/book/99/
+    path('book/<int:pk>/', views.BookDetail.as_view(), name='book_detail'),
 
-    #books/author/99/
-    path('author<int:pk>', views.AuthorDetail.as_view(), name='author_detail'),
+    # /books/author/99/
+    path('author/<int:pk>/', views.AuthorDetail.as_view(), name='author_detail'),
 
-    #books/publisher/99/
-    path('publisher<int:pk>', views.PublisherDetail.as_view(), name='publisher_detail')
+    # /books/publisher/99/
+    path('publisher/<int:pk>/', views.PublisherDetail.as_view(), name='publisher_detail'),
 ]
