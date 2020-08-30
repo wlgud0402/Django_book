b116d152df885d7696d6fb6d9e3072cf711053cd
wlgud0402 <wlgudrlgus@naver.com>
2020-08-30 16:34:43 +0900

    extra

--- ch3/ch3/urls.py
+++ ch3/ch3/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from ch3 import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
-    path('books/', include('books.urls'))
+    path('books/', include('books.urls')),
+    path('', views.HomeView.as_view(), name='home')
 ]
