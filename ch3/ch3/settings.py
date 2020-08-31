6a5e9329e96656a930d68915300bd95645f000d5
wlgud0402 <wlgudrlgus@naver.com>
2020-08-31 17:29:23 +0900

    secret_key

--- ch3/ch3/settings.py
+++ ch3/ch3/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = 'u&bkvf(&@od48ms)6uo3d)y!&hvkr$25g--82c3stto)ta*17*'
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
