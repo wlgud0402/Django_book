3852eaafa9ae266ffde71d3988db704e2adba7f5
wlgud0402 <wlgudrlgus@naver.com>
2020-09-03 17:53:29 +0900

    data migrate to mysql

--- ch3/ch3/settings.py
+++ ch3/ch3/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'ch3.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodb',
+        'USER':'root',
+        'PASSWORD':'123',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
     }
 }
 
